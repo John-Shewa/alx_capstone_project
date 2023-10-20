a31f10f514f5fb63f3c5c358aa5f0eab172e3ee3
Yohannes Shewakena <yohannessh@dbe.com>
2023-10-20 17:30:45 +0300

    Budget Tracker

--- budget.js
+++ budget.js
@@ -1,48 +1,83 @@
-// Take Input value from user and save it to input variable
-const incomeInput = document.querySelector('input[name="income"]');
-const input = incomeInput.value;
-
-// Take expense value of rent from user and save it to rent variable
-const rentExpense = document.querySelector('input[name"rent"]');
-const rent = rentExpense.value;
-
-// Take expense value of food from user and save it to food variable
-const foodExpense = document.querySelector('input[name"Food"]');
-const food = foodExpense.value;
-
-// Take expense value of Transportation from user and save it to transport variable
-const TransportExpense = document.querySelector('input[name"Transport"]');
-const transport = TransportExpense.value;
-
-// Take expense value of utility from user and save it to utility variable
-const utilityExpense = document.querySelector('input[name"Utility"]');
-const utility = utilityExpense.value;
-
 // A function to add a form for other expenses
+// Get form element
+const form = document.getElementById('expenseform');
+// Get button element
+const button = document.getElementById('dropdown-button');
+// Add event listener to the button
+button.addEventListener('click', function(){
+    // Ask user to input an expense he/she wants to add
+    const labelName = prompt('Enter name of other expense: ');
 
-const dropdownButton = document.querySelector('#dropdown-button');
-const expenseForm = document.querySelector('#expense-form');
+    // create new input element
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.name = 'otherexpense';
+    input.placeholder = 'Enter other expense';
 
-dropdownButton.addEventListener('click', function(){
-    const expenseName = expenseForm.querySelector('input[name="other expense"]').value;
-    const expenseValue = expenseForm.querySelector('input[name="value"]').value;
-    expenseForm.style.display = 'block';
-});
+    // Add a class to the output to style with css
+    input.classList.add('new-form');
 
-expenseForm.addEventListener('submit', function(event){
-    event.preventDefault();
+    // create new lebel element
+    const label = document.createElement('label');
+    label.htmlFor = 'Otherexpense';
+    label.textContent = `${labelName} `;
 
-const expenseInput = document.createElement('input');
+    //create a new div element to wrap the label, input, and delete button elements
+    const div = document.createElement('div');
+    div.classList.add('form-group');
+    div.appendChild(label);
+    div.appendChild(input);
 
-expenseInput.type = 'number';
-expenseInput.name = expenseName;
-expenseInput.value = expenseValue;
+    //create a delete button element to remove added expense
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function(){
+        div.remove();
+    });
 
-document.querySelector('form.form').appendchild(expenseInput);
+    //Append the lebel, input and delete button elements to the div
+    div.appendChild(deleteButton);
 
-expenseForm.style.display = 'none';
+    //Append the new div element to the form
+    form.appendChild(div);
 })
 
-// Total expense
+//Get the income and expense input fields
+const incomeInput = document.querySelector('input[name="Income"]');
+const rentInput = document.querySelector('input[name="Rent"]');
+const foodInput = document.querySelector('input[name="Food"]');
+const utilityInput = document.querySelector('input[name="Utility"]');
+const TransportationInput = document.querySelector('input[name="Transportation"]');
+const otherExpenseInputs = document.querySelectorAll('input[name="otherexpense"]');
 
-const Totalexpense = rent + food + transport + utility + expenseValue;
+//get the graph section element
+const graphSection = document.querySelector('.graph-section');
+
+//add an event listener to the form to calculate total income and expenses
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //calculate total income
+    const totalIncome = parseFloat(incomeInput.value);
+    //calculate total expenses
+    const rentExpense = parseFloat(rentInput.value);
+    const foodExpense = parseFloat(foodInput.value);
+    const utilityExpense = parseFloat(utilityInput.value);
+    const transportationExpense = parseFloat(TransportationInput.value);
+    let otherExpensesTotal = 0;
+    otherExpenseInputs.forEach((input) => {
+        if (input.value) {
+            otherExpensesTotal += parseFloat(input.value);
+        }
+    });
+    const totalExpenses = rentExpense + foodExpense + utilityExpense + transportationExpense + otherExpensesTotal;
+
+    //display total income and expenses in the graph section
+    graphSection.innerHTML = `
+    <h2>Total Income: $${totalIncome.toFixed(2)}</h2>
+    <h2>Total Expenses: $${totalExpenses.toFixed(2)}</h2>
+    `;
+})
+
+
+
+
